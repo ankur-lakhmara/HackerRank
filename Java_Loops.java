30b9aa26aaf218b358209ba072f0466fef400393
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2021-12-11 10:34:50 +0530

    Java_Loops.java

---
+++ Java_Loops.java
@@ -0,0 +1,35 @@
+//problem link https://www.hackerrank.com/challenges/java-loops/problem
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int calc = a;
+            for(int j=0;j<n;j++){
+                calc+=(Math.pow(2,j)*b);
+                System.out.print(calc+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
+/*
+Input (stdin)
+
+Download
+2
+0 2 10
+5 3 5
+Expected Output
+
+Download
+2 6 14 30 62 126 254 510 1022 2046
+8 14 26 50 98
+*/
